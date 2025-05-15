0cc4c6c638a18af7721a14f05bd880158cc96e4d
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2025-05-15 23:29:47 +0530

    Evrerything done

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://nishmas-restaurant.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
